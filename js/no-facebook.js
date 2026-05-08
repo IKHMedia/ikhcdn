420ccbccb0511259d55416c850280bf8c813fc82
IKHMedia <ian@ikhmedia.com>
2026-05-08 11:18:40 -0500

    update

---
+++ js/no-facebook.js
@@ -0,0 +1,8 @@
+(function() {
+  const url = new URL(window.location.href);
+  if (url.searchParams.has('fbclid')) {
+    url.searchParams.delete('fbclid');
+    // Update the address bar without reloading
+    window.history.replaceState(null, '', url.toString());
+  }
+})();
